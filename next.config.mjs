f5128ad0b9f0f8657936c0ef03493e3ec66f1757
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2024-08-19 13:52:44 +0330

    ewf

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
     experimental: {
         taint: false,
     },
-    env: {
-        "ProductionURL":"https://digizoneshop.com",
-"DevURL": "http://localhost:3000",
-        "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY":"pk_test_ZXRlcm5hbC10ZXJtaXRlLTk1LmNsZXJrLmFjY291bnRzLmRldiQ",
-        "CLERK_SECRET_KEY":"sk_test_nZKpfnvNslu1nisecmXaRNRYmEL3bFWSjLZA1CMF6b"
-    },
+//     env: {
+//         "ProductionURL":"https://digizoneshop.com",
+// "DevURL": "http://localhost:3000",
+//         // "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY":"pk_test_ZXRlcm5hbC10ZXJtaXRlLTk1LmNsZXJrLmFjY291bnRzLmRldiQ",
+//         // "CLERK_SECRET_KEY":"sk_test_nZKpfnvNslu1nisecmXaRNRYmEL3bFWSjLZA1CMF6b"
+//     },
 };
 
 export default nextConfig;
