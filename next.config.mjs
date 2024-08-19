ac0cda87d30f06e19ef81dc1a248cf615b7e9aa5
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2024-08-19 13:50:47 +0330

    ewf

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ const nextConfig = {
     env: {
         "ProductionURL":"https://digizoneshop.com",
 "DevURL": "http://localhost:3000",
+        "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY":"pk_test_ZXRlcm5hbC10ZXJtaXRlLTk1LmNsZXJrLmFjY291bnRzLmRldiQ",
+        "CLERK_SECRET_KEY":"sk_test_nZKpfnvNslu1nisecmXaRNRYmEL3bFWSjLZA1CMF6b"
     },
 };
 
